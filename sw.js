47965d0f5be86a1a925c1bacb14f4f6d3960f6bd
Geethika <geethikaannam718@gmail.com>
2026-09-06 15:47:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,163 @@
+const CACHE_NAME =
+    "birthdayverse-part8a-v1";
 
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./script.js",
+
+    "./manifest.json"
+
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            )
+            .then(
+                cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+            )
+
+        );
+
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+                .then(
+                    keys =>
+                        Promise.all(
+
+                            keys
+                                .filter(
+                                    key =>
+                                        key !==
+                                        CACHE_NAME
+                                )
+                                .map(
+                                    key =>
+                                        caches.delete(
+                                            key
+                                        )
+                                )
+
+                        )
+                )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(
+                cachedResponse => {
+
+                    if (
+                        cachedResponse
+                    ) {
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(
+                        event.request
+                    )
+                    .then(
+                        networkResponse => {
+
+                            if (
+                                !networkResponse ||
+                                networkResponse.status !== 200 ||
+                                networkResponse.type ===
+                                "opaque"
+                            ) {
+
+                                return networkResponse;
+
+                            }
+
+
+                            const response =
+                                networkResponse.clone();
+
+
+                            caches.open(
+                                CACHE_NAME
+                            )
+                            .then(
+                                cache =>
+                                    cache.put(
+                                        event.request,
+                                        response
+                                    )
+                            );
+
+
+                            return networkResponse;
+
+                        }
+                    )
+                    .catch(
+                        () =>
+                            caches.match(
+                                "./index.html"
+                            )
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
